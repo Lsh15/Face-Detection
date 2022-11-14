bd061406dec5110565daf1b427286741cedc4777
SeunghHun <starzary@naver.com>
2022-11-14 11:09:22 +0900

    Add FaceGraphic.kt

---
+++ app/src/main/java/com/example/facedetection/FaceGraphic.kt
@@ -0,0 +1,8 @@
+package com.example.facedetection
+
+import android.graphics.*
+import com.google.firebase.ml.vision.face.FirebaseVisionFace
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceLandmark
+
+class FaceGraphic  {
+}
